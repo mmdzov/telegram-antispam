7e21600fa6e6db30da4f1246dde649f30af20311
mmdzov <mzov939@gmail.com>
2021-08-28 18:27:11 +0430

    finished

--- inline/inline_main.js
+++ inline/inline_main.js
@@ -4,8 +4,8 @@ const { callback: b } = Markup.button;
 let inlineMain = Markup.inlineKeyboard([
   [b("تنظیمات ⚙️", "settings")],
   [b("مدیریت 👮🏻‍♂", "panel")],
-  [b("خودکار 🤖", "automate")],
-  [b("آنالیز 📊", "analysis")],
+  // [b("خودکار 🤖", "automate")],
+  // [b("آنالیز 📊", "analysis")],
 ]);
 
 module.exports = inlineMain;
